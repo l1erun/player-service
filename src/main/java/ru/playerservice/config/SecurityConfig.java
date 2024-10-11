b85f73200ba5cd6d42b81a361db9ed9440dd9b65
Алексей Степанов <aleksejstepanov@MacBook-Air-Aleksej.local>
2024-10-11 13:55:11 +0300

    Переделал работу swagger, добавил jwtdecoder

--- src/main/java/ru/playerservice/config/SecurityConfig.java
+++ src/main/java/ru/playerservice/config/SecurityConfig.java
@@ -1,19 +1,23 @@
 package ru.playerservice.config;
 
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.web.SecurityFilterChain;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import javax.crypto.SecretKey;
+
 @Configuration
-//@EnableWebSecurity
 public class SecurityConfig {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,21 +26,16 @@ public class SecurityConfig {
                         .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
                         .anyRequest().authenticated()
                 )
-                .csrf(csrf -> csrf.disable());  // Отключаем CSRF, если не нужен
+                .csrf(csrf -> csrf.disable())  // Отключаем CSRF, если не нужен
+                .oauth2ResourceServer(oauth2 -> oauth2
+                        .jwt(Customizer.withDefaults()));  // Новый синтаксис для проверки JWT токенов
         return http.build();
     }
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//        http
-//                .authorizeRequests()
-//                .antMatchers("/players/**").authenticated()
-//                .and()
-//                .oauth2ResourceServer()
-//                .jwt();
-//
-//        return http.build();
-//    }
-
-    // Дополнительная настройка, если необходимо
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        SecretKey key = Keys.hmacShaKeyFor(keyBytes);
+        return NimbusJwtDecoder.withSecretKey(key).build();
+    }
 }
